da23521e6764a37d0e3b8308567a6a8e14d8288d
mateusgabrielsilva <mateus92.mgs@gmail.com>
2022-05-25 11:59:40 -0300

    Meu primeiro app em react native usando expo

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Uhull!</Text>
+      <Text style={styles.text}>Minha primeira aplicação com React Native e Expo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#145DA0',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff'
   },
 });
